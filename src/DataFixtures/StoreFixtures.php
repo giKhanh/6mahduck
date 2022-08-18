b0383ea4f166908b4ce369a7d79d39997927fe20
store <thanhpqgch210568@fpt.edu.vn>
2022-08-18 21:09:11 +0700

    store

--- src/DataFixtures/StoreFixtures.php
+++ src/DataFixtures/StoreFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Store;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class StoreFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i=1; $i<=5; $i++) {
+            $store = new Store;
+            $store->setName("Store $i")
+                ->setImage("https://img.freepik.com/premium-vector/retro-science-education-background_23-2148476365.jpg?w=2000");
+            $manager->persist($store);
+        }
 
         $manager->flush();
     }
